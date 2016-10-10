83432751d929ea77a30f64041a714af077c7ea7e
龙清岭 <l_lori@163.com>
2016-10-10 14:57:20 +0800

    初始化项目

---
+++ QLDouYu/QLDouYu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QLDouYu
+//
+//  Created by 龙清岭 on 2016/10/10.
+//  Copyright © 2016年 longqingling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
